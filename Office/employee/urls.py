5ae1b15e74b03e1fe5bcf6b952cd65df7010702e
Ashish Bhardwaj <erashish41@gmail.com>
2025-10-08 23:11:46 +0530

    views, urls and templates updated

--- Office/employee/urls.py
+++ Office/employee/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from employee.views import company
 
 urlpatterns = [
-    
+    path('', company, name='employee_list')
 ]
--- Office/employee/views.py
+++ Office/employee/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from employee.models import Company, Employee, Project, Department
 
 # Create your views here.
+
+def company(request):
+    return render(request, 'base.html')
