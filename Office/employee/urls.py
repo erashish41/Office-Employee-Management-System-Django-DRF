dc44de344c5ba9729688b65f87b334d6e78de5cb
Ashish Bhardwaj <erashish41@gmail.com>
2025-10-21 19:08:57 +0530

    company details rendered

--- Office/employee/urls.py
+++ Office/employee/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from employee.views import companies
+from employee.views import companies, company_detail
 
 urlpatterns = [
-    path('companies/', companies, name='company_list')
+    path('companies/', companies, name='company_list'),
+    path('company_detail/<uuid:pk>/', company_detail, name='company_detail')
 ]
--- Office/employee/views.py
+++ Office/employee/views.py
@@ -10,7 +10,12 @@ print("employee views loaded 1")
 def companies(request):
     print("all companies data")
     companies = Company.objects.all()
-    context = {'companies' : companies}
+    context = {'all_companies' : companies}
     return render(request, "company_list.html", context)
 
 print("employee views loaded 2")
+def company_detail(request, pk):
+    print("company details")
+    company = Company.objects.get(id=pk)
+    context = {'company': company}
+    return render(request, "company_detail.html", context)
